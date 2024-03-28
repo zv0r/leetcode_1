fc0e3816da872ba87d337ee5f71c27f35b4397d8
Andrey Zvorygin <zv0r@ya.ru>
2024-03-28 17:42:34 +0300

    Make another not so dumb solution

--- src/go/main.go
+++ src/go/main.go
@@ -11,22 +11,14 @@ func main() {
 func twoSum(nums []int, target int) []int {
 	var result []int = make([]int, 2)
 	var positions map[int]int = make(map[int]int)
-	var dummy []int
 
 	for i := 0; i < len(nums); i++ {
-		positions[nums[i]] = i
-		if nums[i]+nums[i] == target {
-			dummy = append(dummy, i)
-			if len(dummy) == 2 {
-				result = dummy
-				break
-			}
-		}
-		if val, ok := positions[target-nums[i]]; ok && val != i {
-			result[0] = positions[nums[i]]
+		if _, ok := positions[target-nums[i]]; ok {
+			result[0] = i
 			result[1] = positions[target-nums[i]]
 			break
 		}
+		positions[nums[i]] = i
 	}
 
 	return result
